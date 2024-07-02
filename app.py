4f853a288e65b639b87bac826ea828f794040803
gillevinz <gill@wix.com>
2024-07-02 11:02:24 +0300

    first draft

---
+++ app.py
@@ -0,0 +1,41 @@
+import logging
+from datetime import datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from app import create_app
+from app.services.openai_service import get_llm_message_to_push
+from app.utils.whatsapp_utils import send_message
+
+
+# TEMP TEST
+def morning_task(name, number):
+    """Task to be run every morning at a scheduled time."""
+    current_time = datetime.now()
+    logging.info(f"Morning task running at: {current_time}")
+
+    prompt = (
+        f"Today is {current_time}. "
+        "Wish Sarah a good day and remind her of the upcoming tasks."
+    )
+    #llm_msg = get_llm_message_to_push(prompt, number, name)
+    llm_msg = "test"
+    send_message(llm_msg)
+
+
+def main():
+    app = create_app()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"]), 'cron', hour=10, minute=0)
+    scheduler.start()
+    morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"])
+    logging.info("Scheduler started and Flask app initialized")
+    try:
+        app.run(host="0.0.0.0", port=8000)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        logging.info("Scheduler and Flask app stopped")
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/services/openai_service.py
@@ -0,0 +1,98 @@
+import logging
+import os
+import shelve
+import time
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
+client = OpenAI(api_key=OPENAI_API_KEY)
+#client = OpenAI(api_key=current_app.config.get("OPENAI_API_KEY"))
+
+
+# Use context manager to ensure the shelf file is closed properly
+def check_if_thread_exists(wa_id):
+    with shelve.open("threads_db") as threads_shelf:
+        return threads_shelf.get(wa_id, None)
+
+
+def store_thread(wa_id, thread_id):
+    with shelve.open("threads_db", writeback=True) as threads_shelf:
+        threads_shelf[wa_id] = thread_id
+
+
+def run_assistant(thread, name):
+    # Retrieve the Assistant
+    assistant = client.beta.assistants.retrieve(OPENAI_ASSISTANT_ID)
+
+    # Run the assistant
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        # instructions=f"You are having a conversation with {name}",
+    )
+
+    # Wait for completion
+    # https://platform.openai.com/docs/assistants/how-it-works/runs-and-run-steps#:~:text=under%20failed_at.-,Polling%20for%20updates,-In%20order%20to
+    while run.status != "completed":
+        # Be nice to the API
+        time.sleep(0.5)
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+
+    # Retrieve the Messages
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    new_message = messages.data[0].content[0].text.value
+    logging.info(f"Generated message: {new_message}")
+    return new_message
+
+
+def generate_llm_response(message_body, wa_id, name):
+    thread, thread_id = get_thread(name, wa_id)
+
+    # Add message to thread
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message_body,
+    )
+
+    # Run the assistant and get the new message
+    new_message = run_assistant(thread, name)
+
+    return new_message
+
+
+def get_llm_message_to_push(message_body, wa_id, name):
+    thread, thread_id = get_thread(name, wa_id)
+
+    # Add message to thread
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message_body,
+    )
+
+    # Run the assistant and get the new message
+    new_message = run_assistant(thread, name)
+    return new_message
+
+
+def get_thread(name, wa_id):
+    # Check if there is already a thread_id for the wa_id
+    thread_id = check_if_thread_exists(wa_id)
+    # If a thread doesn't exist, create one and store it
+    if thread_id is None:
+        logging.info(f"Creating new thread for {name} with wa_id {wa_id}")
+        thread = client.beta.threads.create()
+        store_thread(wa_id, thread.id)
+        thread_id = thread.id
+
+    # Otherwise, retrieve the existing thread
+    else:
+        logging.info(f"Retrieving existing thread for {name} with wa_id {wa_id}")
+        thread = client.beta.threads.retrieve(thread_id)
+    return thread, thread_id
+
