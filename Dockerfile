1d3ee011bb6a211857e95e5455e242af2f2a2231
gillevinz <gill@wix.com>
2024-07-06 19:13:02 +0300

    docker try

--- Dockerfile
+++ Dockerfile
@@ -21,4 +21,4 @@ ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
 
 # Run the application
-CMD ["flask", "run"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from app import create_app
 from app.services.openai_service import get_llm_message_to_push
 from app.utils.whatsapp_utils import send_message
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 # TEMP TEST
@@ -19,7 +22,7 @@ def morning_task(name, number):
         f"Today is {current_time}. "
         "Wish Sarah a good day and remind her of the upcoming tasks."
     )
-    #llm_msg = get_llm_message_to_push(prompt, number, name)
+    # llm_msg = get_llm_message_to_push(prompt, number, name)
     llm_msg = "test"
     send_message(llm_msg)
 
@@ -33,16 +36,18 @@ def main():
         return jsonify({"message": "Hello, World!"})
 
     scheduler = BackgroundScheduler()
-    #scheduler.add_job(morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"]), 'cron', hour=10, minute=0)
+    # scheduler.add_job(morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"]), 'cron', hour=10, minute=0)
     scheduler.start()
     send_message("app started")
-    #morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"])
+    # morning_task(app.config["TEST_NAME"], app.config["TEST_NUMBER"])
     logging.info("Scheduler started and Flask app initialized")
     try:
         app.run(host="0.0.0.0", port=8000)
-    except (KeyboardInterrupt, SystemExit):
+        logging.info("App started!")
+    except Exception as e:
         scheduler.shutdown()
-        logging.info("Scheduler and Flask app stopped")
+        logging.error(f"An error occurred: {str(e)}")
+        raise
 
 
 if __name__ == "__main__":
